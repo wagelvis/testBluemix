c50219485029e49237bf8e370cd4357170964cbc
Wilmer Hernández <wagelvis@95.113.153.190.net-uno.net>
2017-01-04 17:45:07 -0400

    Update chatbox title

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
                 <div class="col-md-6 col-md-offset-3">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
-                            <span class="glyphicon glyphicon-comment"></span> Chat Firebase
+                            <span class="glyphicon glyphicon-comment"></span> BLUEMIX - Chat Firebase
                         </div>
                         <div class="panel-body">
                             <ul class="chat"> </ul>
